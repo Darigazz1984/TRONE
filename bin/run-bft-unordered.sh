d13ae8d3938a1d19a931450ea30b18dad0c8f4ba
igor <igor.antunes1984@gmail.com>
2012-11-05 16:19:03 +0000

    Demo Novembro com painel de controlo a funcionar, clientes representados por velocimentros

---
+++ bin/run-bft-unordered.sh
@@ -0,0 +1,2 @@
+./run-subscribers.sh 1 1 0 background dhcp
+./run-publishers.sh 1 1 5 background dhcp
---
+++ bin/run-cft.sh
@@ -0,0 +1,2 @@
+./run-subscribers.sh 1 1 0 background logs dns
+./run-publishers.sh 1 1 5 background logs dns
